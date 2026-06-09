e82a84c8198bf65b4cef5380ece0e82b052736c3
LuisG <luisguillermoa8@gmail.com>
2026-06-09 17:37:20 -0500

    feat: add SmartRent menu and user listing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { User } from './components/user/user';
+import { MenuComponent } from './components/menu-component/menu-component';
+import { ComponentList } from './components/user/component-list/component-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent
+  },
+  {
+    path: 'users',
+    component: User,
+    children: [
+      {
+        path: 'list',
+        component: ComponentList
+      }
+    ]
+  }
+];
---
+++ src/app/models/User.ts
@@ -0,0 +1,13 @@
+export class User{
+    name: string = ''
+    lastName: string = ''
+    dni: number = 0
+    username: string = ''
+    statusVerification: boolean = false
+    profilePhoto: string = ''
+    phoneNumber: string = ''
+    createdDate: Date = new Date()
+    updateDate: Date = new Date()
+    enabled: boolean = false
+    role: string = ''
+}
