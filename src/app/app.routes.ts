4eba11e7087068cc4681bf474d63392855385377
ruthantonnn28 <ruthantonnn24@gmail.com>
2026-06-10 00:17:09 -0500

    List Estate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { User } from './components/user/user';
 import { MenuComponent } from './components/menu-component/menu-component';
 import { ComponentList } from './components/user/component-list/component-list';
+import { EstateComponent } from './components/estate-component/estate-component';
+import { EstateList } from './components/estate-component/estate-list/estate-list';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,16 @@ export const routes: Routes = [
       {
         path: 'list',
         component: ComponentList
+      }
+    ]
+  },
+  {
+    path: 'estates',
+    component: EstateComponent,
+    children: [
+      {
+        path: 'list',
+        component: EstateList
       }
     ]
   }
---
+++ src/app/models/Estate.ts
@@ -0,0 +1,19 @@
+import { User } from "./User"
+
+export class Estate{
+    idEstate: number = 0
+    title: string =''
+    description: string=''
+    adress: string=''
+    district:string=''
+    city:string=''
+    monthlyPrice: number = 0
+    type: string=''
+    state: boolean=false
+    rooms:number=0
+    bathrooms:number=0
+    areaM2:number=0
+    creationDate:Date=new Date()
+    idUser:User = new User()
+}
+
