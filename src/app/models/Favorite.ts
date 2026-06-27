40a3403d709402c325a8f1e46992e64f572569d7
LuisG <luisguillermoa8@gmail.com>
2026-06-26 20:26:11 -0500

    Recorte proyecto

--- src/app/models/Favorite.ts
+++
@@ -1,8 +0,0 @@
-export class Favorite {
-  idFavorite: number = 0;
-  creationDate: string = '';
-  idUser: number = 0;
-  idEstate: number = 0;
-  user?: { idUser: number; username?: string };
-  estate?: { idEstate: number; title?: string; location?: string };
-}
