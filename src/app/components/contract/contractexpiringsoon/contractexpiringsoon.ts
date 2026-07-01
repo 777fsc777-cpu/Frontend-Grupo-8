96e3484e9d3d698abdba2e385ec2e862d678a38a
777fsc777-cpu <777fsc777@gmail.com>
2026-07-01 10:59:41 -0500

    implementacion del metodo contract expiring soon

---
+++ src/app/components/contract/contractexpiringsoon/contractexpiringsoon.ts
@@ -0,0 +1,28 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ContractExpiringDTO } from '../../../models/contract-expiring-dto';
+import { Contractservice } from '../../../services/contractservice';
+
+@Component({
+  selector: 'app-contractexpiringsoon',
+  imports: [MatCardModule, MatIconModule, DatePipe],
+  templateUrl: './contractexpiringsoon.html',
+  styleUrl: './contractexpiringsoon.css',
+})
+export class Contractexpiringsoon {
+  contratos: ContractExpiringDTO[] = [];
+ 
+  constructor(private cS: Contractservice) {}
+ 
+  ngOnInit(): void {
+    this.cargarContratosPorVencer();
+  }
+ 
+  cargarContratosPorVencer() {
+    this.cS.expiringSoon().subscribe((data) => {
+      this.contratos = data;
+    });
+  }
+}
